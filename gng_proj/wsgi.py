dfae02d23b14d55a4c9f6c43d5f2e04a3de41c3b
mohannad saadeh <155642672+MohnadSaadeh@users.noreply.github.com>
2024-04-07 00:48:21 +0200

    name chande to gng_proj

--- gng_proj/wsgi.py
+++ gng_proj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GNG_Django project.
+WSGI config for gng_proj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GNG_Django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gng_proj.settings')
 
 application = get_wsgi_application()
